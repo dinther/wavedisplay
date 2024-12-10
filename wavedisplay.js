f1a1b015946e8d09fc98c6822cc4c5bbe06e5a76
Paul van Dinther <vandinther@gmail.com>
2024-12-11 06:48:48 +1300

    tidyup added getIndex and getSeconds

--- wavedisplay.js
+++ wavedisplay.js
@@ -9,21 +9,20 @@ export class WaveDisplay{
 
     #svg;
     #scrollbar;
-    #minValue=null;
-    #maxValue=null;
-
+    #data;
     #samplesPerPixel;
     #zoom = 1;
     #viewBox = {xmin:0, ymin:0, xmax:100, ymax:100};
     constructor(options){
         this.#options = {...this.#options, ...options};
+        this.#data = this.#options.data;
         this.#svg = this.#createSVG(this.#options.parent);
-        this.#scrollbar = this.#createScrollbar(this.#options.parent);
+        this.#scrollbar = this.#createScrollbar();
         this.#scrollbar.children[0].style.width = (zoom.value * 100) + '%';
         this.#drawValues();
     }
 
-    #createSVG(parent){
+    #createSVG(){
         const svg  = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         svg.classList.add('values-svg');
         const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
@@ -36,7 +35,7 @@ export class WaveDisplay{
     }
 
     #handleWheel(e){
-        let lockIndex = this.#samplesPerPixel * (e.clientX + this.#scrollbar.scrollLeft);
+        let lockIndex = this.getIndex(e.clientX);
         let zoom = Math.min(100, Math.max(1, this.#zoom * Math.exp(-e.deltaY / 80 * this.#options.zoomRate)));  
         this.#scrollZoom(zoom);
         this.#scrollbar.scrollLeft  = (lockIndex - (e.clientX * this.#samplesPerPixel)) / this.#samplesPerPixel;
@@ -48,12 +47,12 @@ export class WaveDisplay{
         this.#scrollZoom(this.#zoom);
     }
 
-    #createScrollbar(parent){
+    #createScrollbar(){
         let scrollbar = document.createElement('div');
         scrollbar.className = 'scrollbar';
         let scrollbarRange = document.createElement('div');
         scrollbar.appendChild(scrollbarRange);
-        parent.appendChild(scrollbar);
+        this.#options.parent.appendChild(scrollbar);
         scrollbar.addEventListener('scroll', (e)=>{
             this.#scrollZoom(this.#zoom);
         });
@@ -63,65 +62,46 @@ export class WaveDisplay{
     #scrollZoom(zoom){
         this.#zoom = Math.abs(zoom);
         this.#scrollbar.children[0].style.width = (this.#zoom * 100) + '%';
-        let offset = this.#scrollbar.scrollLeft / this.#scrollbar.scrollWidth * this.#options.data.length;
-        this.#setViewBox(offset, null, this.#options.data.length / this.#zoom, null);
+        let offset = this.#scrollbar.scrollLeft / this.#scrollbar.scrollWidth * this.#data.length;
+        this.#setViewBox(offset, null, this.#data.length / this.#zoom, null);
     }
 
     #setViewBox(xmin, ymin, xmax, ymax){
-        this.#viewBox.xmin = xmin!=null? Math.floor(xmin) : this.#viewBox.xmin;
-        this.#viewBox.ymin = ymin!=null? Math.floor(ymin) : this.#viewBox.ymin;
-        this.#viewBox.xmax = xmax!=null? Math.floor(xmax) : this.#viewBox.xmax;
-        this.#viewBox.ymax = ymax!=null? Math.floor(ymax) : this.#viewBox.ymax;
-        this.#samplesPerPixel = this.#options.data.length / this.#scrollbar.children[0].offsetWidth;
+        this.#viewBox.xmin = xmin!=null? xmin : this.#viewBox.xmin;
+        this.#viewBox.ymin = ymin!=null? ymin : this.#viewBox.ymin;
+        this.#viewBox.xmax = xmax!=null? xmax : this.#viewBox.xmax;
+        this.#viewBox.ymax = ymax!=null? ymax : this.#viewBox.ymax;
+        this.#samplesPerPixel = this.#data.length / this.#scrollbar.children[0].offsetWidth;
         this.#svg.setAttribute('viewBox', this.#viewBox.xmin + ' ' +this.#viewBox.ymin + ' ' +this.#viewBox.xmax + ' ' + this.#viewBox.ymax);
     }
 
-    #filterData(data) {
-        const blockSize = this.#options.samplesPerPoint;
-        const filteredData = [];
-        for (let i = 0; i < samples; i++) {
-            let blockStart = blockSize * i;
-            let sum = 0;
-            for (let j = 0; j < blockSize; j++) {
-                sum = sum + Math.abs(data[blockStart + j]);
-            }
-            filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
-        }
-        return filteredData;
-    };
-
-    #normalizeData(data){
-        const multiplier = Math.pow(Math.max(...data), -1);
-        return data.map(n => n * multiplier);
-    }
-
-    #normalizeData1(data){
-        let maxV = 0;
-        for (let i=0; i<data.length; i++){
-            maxV = Math.max(maxV, Math.abs(data[i]));
-        }
-        let f = 1/maxV;
-        return data.map(n => n * f);
-    }
-
     #drawValues(){
-        let samples = this.#options.data.length / this.#options.samplesPerPoint;
+        let samples = this.#data.length / this.#options.samplesPerPoint;
         let h = this.#svg.parentElement.offsetHeight;
-        const sampleStep = Math.floor(this.#options.data.length / samples);
+        const sampleStep = Math.floor(this.#data.length / samples);
         let path = 'M0 100';
         path += 'L';
-        for (let i = 0; i < this.#options.data.length; i+=sampleStep) {
-            let value = (0.5 * h) + (this.#options.data[i] * h);   
-            this.#minValue = this.#minValue == null? value : Math.min(this.#minValue, value); 
-            this.#maxValue = this.#maxValue == null? value : Math.max(this.#maxValue, value);
+        let minValue=null;
+        let maxValue=null;
+        for (let i = 0; i < this.#data.length; i+=sampleStep) {
+            let value = 128 + this.#data[i] * 128;   
+            minValue = minValue == null? value : Math.min(minValue, value); 
+            maxValue = maxValue == null? value : Math.max(maxValue, value);
             path += i.toFixed(0) + ' ' + value.toFixed(0) + ' ';
         }
-        this.#setViewBox(0, this.#minValue, this.#options.data.length, this.#maxValue);
+        this.#setViewBox(0, minValue, this.#data.length, maxValue);
         this.#svg.querySelector('path').setAttribute('d', path);
         this.#svg.setAttribute('preserveAspectRatio', 'none');
     }
 
-    #indexToTime
+    getIndex(x){
+        return this.#samplesPerPixel * (x + this.#scrollbar.scrollLeft);
+    }
+
+    getSeconds(x){
+        return this.getIndex(x) / this.#options.sampleRate;
+    }
+
     zoom(value){
         this.#scrollZoom(value);
     }
